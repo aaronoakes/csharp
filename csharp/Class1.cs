3613717fb63fd36b9b8faf7e44403b0d5da2b75a
DESKTOP-AEI9OVN\ahega <aaron.oakes@students.plymouth.ac.uk>
2020-10-12 11:40:13 +0100

    Add project files.

---
+++ csharp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Class1
+    {
+    }
+}
+
+namespace Exercise
+{
+    public class Program
+    {
+        public string SayHello()
+        {
+            return "Hello World!";
+        }
+        public string SayGoodBye()
+        {
+            return ("Hello World!");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+
+            Console.WriteLine(prog.SayHello());
+            Console.WriteLine(prog.SayGoodBye());
+        }
+    }
+}
